b4c2b933ff6e8dcac4fcea6e021d5b5461e801f1
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-03-21 19:13:20 +0100

    ok

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 "use strict"
-const BASE_URL = 'http://localhost:3000/students';
 
 // fetch(BASE_URL)
 // .then(r => r.json())
@@ -52,17 +51,168 @@ const BASE_URL = 'http://localhost:3000/students';
 // .then(data => console.log(data))
 // .catch(error => console.error(error));
 
-const options = {
-  method: 'PATCH',
-  body: JSON.stringify({
-    age: 100,
-    email: 'feature@gmail.com'
-  }),
-  headers: {
-    "Content-Type": "application/json"
-      }
+// const options = {
+//   method: 'PATCH',
+//   body: JSON.stringify({
+//     age: 100,
+//     email: 'feature@gmail.com'
+//   }),
+//   headers: {
+//     "Content-Type": "application/json"
+//       }
+// };
+// fetch(`${BASE_URL}/3`, options)
+// .then(r => r.json())
+// .then(data => console.log(data))
+// .catch(error => console.error(error));
+
+// GET
+// fetch("https://jsonplaceholder.typicode.com/albums/85")
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+
+// POST
+// const postAdd = {
+//     author: "Anna",
+//     body: "CRUD",
+// }
+
+// const options = {
+//   method: "POST",
+//     body: JSON.stringify(postAdd),
+//     headers: {
+//       "Content-Type": "application/json"
+//   }
+// };
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({
+//     author: "Anna",
+//       body: "CRUD",
+//       title: 'Title',
+//     userId:100,
+//   }),
+// })
+//   .then((r) => r.json())
+//   .then((post) => console.log(post))
+//   .catch((error) => console.log(error)); //{author: 'Anna', body: 'CRUD', id: 101}
+
+
+// PUT
+// const postAdd = {
+//     author: "Anna",
+//     body: "CRUD new",
+//     id: 2,
+// }
+
+// const options = {
+//   method: "PATCH",
+//     body: JSON.stringify(postAdd),
+//     headers: {
+//       "Content-Type": "application/json"
+//   }
+// };
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postAdd.id}`, options)
+//     .then(r => r.json())
+//     .then(post => console.log(post))
+//     .catch(error => console.log(error));
+    //{userId: 1, id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'CRUD new', author: 'Anna'}
+
+
+
+
+// Delete
+// const deletePost = 200;
+// const options = {
+//     method: "DELETE"
+// }
+// fetch(`https://jsonplaceholder.typicode.com/posts/${deletePost}`, options)
+//     .then(() => console.log('Post deleted'))
+//     .catch(error => console.log(error));
+
+const BASE_URL = 'http://localhost:3000/students';
+
+async function getAllStudents(){
+  try{
+    const r = await fetch(BASE_URL);
+    const data = await r.json();
+    console.log(data);
+  } catch(error){
+console.log(error);
+  }
 };
-fetch(`${BASE_URL}/3`, options)
-.then(r => r.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+getAllStudents();
+
+async function getStudentById(){
+  try{
+    const r = await fetch(`${BASE_URL}/${id}`);
+    const data = await r.json();
+    console.log(data);
+  } catch(error){
+    console.log(error);
+  }
+};
+ getStudentById();
+
+async function addStudent(student){
+  try{
+    const r = await fetch(`${BASE_URL}`, {
+      method: `POST`,
+      headers: {
+"Content-Type": 'application/json'
+      },
+      body: JSON.stringify(student),
+    });
+    const data = await r.json();
+    console.log(data);
+  } catch (error){
+    console.log(error);
+  }
+};
+
+async function updateStudent(id, updated){
+  try{
+    const r = await fetch(`${BASE_URL}/${id}`, {
+      method: `PUT`,
+      headers: {
+"Content-Type": 'application/json'
+      },
+      body: JSON.stringify(updated),
+    });
+    const data = await r.json();
+    console.log(data);
+  } catch (error){
+    console.log(error);
+  }
+};
+
+async function updateStudent(id, patched){
+  try{
+    const r = await fetch(`${BASE_URL}/${id}`, {
+      method: `PATCH`,
+      headers: {
+"Content-Type": 'application/json'
+      },
+      body: JSON.stringify(patched),
+    });
+    const data = await r.json();
+    console.log(data);
+  } catch (error){
+    console.log(error);
+  }
+};
+
+async function updateStudent(id){
+  try{
+    const r = await fetch(`${BASE_URL}/${id}`, {
+      method: `DELETE`
+    });
+    console.log(id);
+  } catch (error){
+    console.log(error);
+  }
+};
