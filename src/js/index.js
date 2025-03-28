7303227aaebdcab0de5cfbaa5d481b29ebefb689
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-03-28 19:23:31 +0100

    ok

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 "use strict"
-import { basePost } from "./posts";
+import { basePost } from "./posts.js";
 const BASE_URL = 'http://localhost:3000/posts';
  async function getPosts() {
   try {
@@ -32,6 +32,17 @@ async function createPost(title, content) {
 // Оновлення поста
 async function updatePost(id, title, content) {
   try {
+    const bodyEl = JSON.stringify(new basePost(title, content));
+    const options = {
+      method: 'PATCH',
+      body: bodyEl,
+      headers:{
+        "Content-Type": "application/json"
+      }
+    }
+    const r = fetch(`${BASE_URL}/${id}`, options);
+    const data = await r.json();
+    await startApp();
 
          } catch (error) {
     console.error(error);
@@ -57,12 +68,12 @@ async function createComment(postId, comment) {
 // Оновлення відображення постів на сторінці
 function renderPosts(posts) {
   const markUp = posts.map(post => {
-    const comments = posts.comments;
+    const comments = post.comments;
     return ` <div class="post">
       <h2 class="postTitle">${post.title}</h2>
       <p class="postText">${post.text}</p>
-      <button class="editPostButton" data-id="${post.id}">Редагувати</button>
       <button class="deletePostButton" data-id="${post.id}">Видалити</button>
+      <button class="editPostButton" data-id="${post.id}">Редагувати</button>
       <div class="commentsContainer" data-id="${post.id}">
         <h3>Коментарі:</h3>
         <ul>
@@ -75,20 +86,58 @@ function renderPosts(posts) {
           <button type="submit">Додати коментар</button>
         </form>
       </div>
+      <form class="updatingForm">
+    <input type="text" id="titleEdit" placeholder="Заголовок" required>
+    <textarea id="contentEdit" placeholder="Зміст" required></textarea>
+    <button class="updateButton" data-id="${post.id}">Редагувати</button>
+  </form>
     </div>`;
   });
 document.getElementById('postsContainer').insertAdjacentHTML('beforeend', markUp);
     }
 // Обробник події для створення поста
-document.getElementById('createPostForm').addEventListener('submit', cb);
+ document.getElementById('createPostForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#titleInput').value;
+  const content = document.querySelector('#contentInput').value;
+  createPost(title, content);
+ });
 
-// Обробник події для редагування поста
-document.addEventListener('click', cb);
-// Обробник події для видалення поста
-document.addEventListener('click', cb);
+// // Обробник події для редагування поста
+document.querySelector('#postsContainer').addEventListener('click', (e) => {
+  let currentPostId = null;
+  if (e.target.nodeName !== 'DIV'){
+    currentPostId = e.target.dataset.id;
+    document.querySelector('.updatingForm').style.display = 'block';
+    // document.querySelector('.updateButton').addEventListener('click', () => {
+    //   const title = document.querySelector('#titleEdit').value;
+    //     const content = document.querySelector('#contentEdit').value;
+    //     updatePost(postId, title, content);
+    //     console.log(postId);
+        // return;
+    // });
+    // document.querySelector('.updateButton').addEventListener('click', () => {
+    //   let currentPostId = e.target.closest('.post');
+    //   document.querySelector(`${currentPostId}`)
+    // })
+  }
+});
 
-// Обробник події для додавання коментаря
-document.addEventListener('submit', cb);
+// const editForm = document.querySelector(".updatingForm");
+// editForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   // const id = e.target.contains.;
+// if (e.target.classList.contains())
+//   const title = document.querySelector('#titleEdit').value;
+//   const content = document.querySelector('#contentEdit').value;
+//   updatePost(id, title, content);
+// });
+
+// // Обробник події для видалення поста
+// document.addEventListener('click', cb);
+
+// // Обробник події для додавання коментаря
+// document.addEventListener('submit', cb);
 // Запуск додатку
 async function startApp() {
   const posts = await getPosts();
--- src/js/posts.js
+++ src/js/posts.js
@@ -3,7 +3,7 @@ export const basePost = class Post{
     constructor (title, content, comments){
 this.title = title || '';
 this.text = content || '';
-this.comments = [];
+this.comments = [] || comments;
     }
 
 }
