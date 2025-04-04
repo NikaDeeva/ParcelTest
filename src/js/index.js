6d208db7e93ac9d5b3947c17cd164a85cb21c93a
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-04-04 17:45:52 +0200

    ok

--- src/js/index.js
+++ src/js/index.js
@@ -1,174 +1,142 @@
-"use strict"
+"use strict";
 import { basePost } from "./posts.js";
-const BASE_URL = 'http://localhost:3000/posts';
- async function getPosts() {
+const BASE_URL = "http://localhost:3001/posts";
+
+async function getPosts() {
   try {
     const r = await fetch(BASE_URL);
     const posts = await r.json();
     return posts;
-        } catch (error) {
-    console.error(error);
-  }
-}
-// Створення нового поста
-async function createPost(title, content) {
-  try {
-    const bodyEl = JSON.stringify(new basePost(title, content));
-    const options = {
-      method: 'POST',
-      body: bodyEl,
-      headers:{
-        "Content-Type": "application/json"
-      }
-    }
-    const r = fetch(`${BASE_URL}`, options);
-    const data = await r.json();
-    await startApp();
-        } catch (error) {
-    console.error(error);
-  }
-}
-
-// Оновлення поста
-async function updatePost(id, title, content) {
-  try {
-    const bodyEl = JSON.stringify(new basePost(title, content));
-    const options = {
-      method: 'PATCH',
-      body: bodyEl,
-      headers:{
-        "Content-Type": "application/json"
-      }
-    }
-    const r = fetch(`${BASE_URL}/${id}`, options);
-    const data = await r.json();
-    await startApp();
-
-         } catch (error) {
-    console.error(error);
-  }
-}
-// Видалення поста
-async function deletePost(id) {
-  try {
-    const options = {
-      method: 'DELETE',
-    }
-    const r = fetch(`${BASE_URL}/${id}`, options);
-    const data = await r.json();
-    await startApp();
-         } catch (error) {
-    console.error(error);
-  }
-}
-
-// Додавання коментаря до поста
-async function createComment(postId, comment) {
-  try {
-    const rPost = await fetch(`${BASE_URL}/${postId}`);
-    const data = await rPost.json();
-    data.comments.push({text: comment});
-    console.log(data);
-  
-    const options = {
-      method: 'PATCH',
-      bodyEl: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
-    const r = await fetch(`${BASE_URL}/${postId}`, options);
-    const res = await r.json();
-    await startApp();
-        // if (!comment) {
-    //   console.error("Write a comment");
-    //   return;
-    // }
-    //      const options = {
-    //       method: 'POST',
-    //       body: JSON.stringify({ "content": comment }),
-    //       headers: {
-    //         "Content-Type": "application/json"
-    //       }
-    //      }
-    //      const r = await fetch(`${BASE_URL}/${postId}/comments`, options);
-    //      const data = await r.json();
-    // await startApp();
   } catch (error) {
     console.error(error);
   }
 }
 
-// Оновлення відображення постів на сторінці
+async function createPost(title, content) {
+  try {
+    const bodyEl = JSON.stringify(new basePost(title, content));
+    const options = {
+      method: "POST",
+      body: bodyEl,
+      headers: { "Content-Type": "application/json" },
+    };
+    const r = await fetch(BASE_URL, options);
+    await r.json();
+    await startApp();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function updatePost(id, title, content) {
+  try {
+    const bodyEl = JSON.stringify(new basePost(title, content));
+    const options = {
+      method: "PATCH",
+      body: bodyEl,
+      headers: { "Content-Type": "application/json" },
+    };
+    const r = await fetch(`${BASE_URL}/${id}`, options);
+    await r.json();
+    await startApp();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function deletePost(id) {
+  try {
+    const options = { method: "DELETE" };
+    const r = await fetch(`${BASE_URL}/${id}`, options);
+    await r.json();
+    await startApp();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function createComment(postId, comment) {
+  try {
+    const rPost = await fetch(`${BASE_URL}/${postId}`);
+    const data = await rPost.json();
+    data.comments = data.comments || [];
+    data.comments.push({ text: comment });
+
+    const options = {
+      method: "PATCH",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const r = await fetch(`${BASE_URL}/${postId}`, options);
+    await r.json();
+    await startApp();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 function renderPosts(posts) {
-  const markUp = posts.map(post => {
-    const comments = post.comments;
-    return ` <div class="post">
+  document.getElementById("postsContainer").innerHTML = "";
+  const markUp = posts.map((post) => {
+    const comments = post.comments || [];
+    return `<div class="post">
       <h2 class="postTitle">${post.title}</h2>
       <p class="postText">${post.text}</p>
-      <button class="editPostButton" data-id="${post.id}">Редагувати</button>
-      <button class="deletePostButton" data-id="${post.id}">Видалити</button>
+      <button class="editPostButton" data-id="${post.id} type="button">Редагувати</button>
+      <button class="deletePostButton" data-id="${post.id} type="button">Видалити</button>
         <h3>Коментарі:</h3>
         <ul>
-            ${comments.map(comment => {
-              return `<li>${comment.text}</li>`;
-            }).join("")}
+            ${comments.map(comment => `<li>${comment.text}</li>`).join("")}
         </ul>
-        <form class="createCommentForm">
+        <form class="createCommentForm" data-id="${post.id}">
           <input type="text" class="commentInput" placeholder="Новий коментар" required>
           <button class="addCommentButton" type="submit">Додати коментар</button>
         </form>
     </div>`;
   });
-  document.getElementById('postsContainer').innerHTML = "";
-  document.getElementById('postsContainer').insertAdjacentHTML('beforeend', markUp);
-  
-    }
+  document
+    .getElementById("postsContainer")
+    .insertAdjacentHTML("beforeend", markUp.join(""));
+}
 
- document.getElementById('createPostForm').addEventListener('submit', (e) => {
+document.getElementById("createPostForm").addEventListener("submit", (e) => {
   e.preventDefault();
-  const title = document.querySelector('#titleInput').value;
-  const content = document.querySelector('#contentInput').value;
+  const title = document.querySelector("#titleInput").value;
+  const content = document.querySelector("#contentInput").value;
   createPost(title, content);
- });
-
- document.querySelector('#postsContainer').addEventListener('click', async (e) => {
-  
-  e.preventDefault();
-  let currentPostId = null;
-
-  if (e.target.classList.contains('deletePostButton')) {
-    currentPostId = e.target.dataset.id;
-    await deletePost(currentPostId); 
-  }
-  if (e.target.classList.contains('editPostButton')) {
-    currentPostId = e.target.dataset.id;
-    const title = prompt('New title');
-    const text = prompt('New text');
-    await updatePost(currentPostId, title, text);
-  }
-//   if (e.target.classList.contains('addCommentButton')){
-//     const postEl = e.target.closest('.post');
-//     const elId = postEl.querySelector('[data-id]');
-//     currentPostId = elId.dataset.id;
-//     const comment = postEl.querySelector('.commentInput').value;
-// await createComment(currentPostId, comment);
-// console.log(currentPostId, comment);
-// postEl.querySelector('.commentInput').value = '';
-//   }
 });
 
-document.querySelector('#postsContainer').addEventListener('submit', async (e) => {
+document.querySelector("#postsContainer").addEventListener("click", async (e) => {
   e.preventDefault();
-  let currentPostId = null;
-  const postEl = e.target.closest('.post');
-      const elId = postEl.querySelector('[data-id]');
-      currentPostId = elId.dataset.id;
-      const comment = postEl.querySelector('.commentInput').value;
-  await createComment(currentPostId, comment);
-  console.log(currentPostId, comment);
-  postEl.querySelector('.commentInput').value = '';
-})
+  let currentPostId = e.target.dataset.id;
+
+  if (e.target.classList.contains("deletePostButton")) {
+    await deletePost(currentPostId);
+  }
+
+  if (e.target.classList.contains("editPostButton")) {
+    const title = prompt("New title", "Новий заголовок");
+    const text = prompt("New text", "Новий текст");
+    if (title && text) {
+      await updatePost(currentPostId, title, text);
+    }
+  }
+});
+
+document.querySelector("#postsContainer").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("createCommentForm")) {
+    const currentPostId = e.target.dataset.id;
+    const commentInput = e.target.querySelector(".commentInput");
+    const comment = commentInput.value;
+    console.log(comment)
+    if (comment) {
+      await createComment(currentPostId, comment);
+      commentInput.value = "";
+    }
+  }
+});
 
 async function startApp() {
   const posts = await getPosts();
